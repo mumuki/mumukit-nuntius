edcc545366d4fc5d58775856db8b39c90bb61ece
Agustin Pina <aguspina87@gmail.com>
2016-04-07 17:22:34 -0300

    Add consumer logic

---
+++ lib/mumukit/nuntius/consumer.rb
@@ -0,0 +1,20 @@
+class Mumukit::Nuntius::Consumer
+
+	class << self
+
+    def start(queue_name)
+      connection, channel, queue = Mumukit::Nuntius::Connection.start(queue_name)
+      channel.prefetch(1)
+
+      queue.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
+        begin
+          yield delivery_info, properties, body
+          channel.ack(delivery_info.delivery_tag)
+        rescue Interrupt => _
+          channel.close
+          connection.close
+        end
+      end
+    end
+	end
+end
