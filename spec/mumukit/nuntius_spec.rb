88dc0ddce5733b91a4503c3e41b3a32fcfffd307
Federico Scarpa <fedescarpa@gmail.com>
2016-04-29 13:05:46 -0300

    removing dummy test

--- spec/mumukit/nuntius_spec.rb
+++ spec/mumukit/nuntius_spec.rb
@@ -4,8 +4,4 @@ describe Mumukit::Nuntius do
   it 'has a version number' do
     expect(Mumukit::Nuntius::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
