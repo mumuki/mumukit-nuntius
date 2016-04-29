8b93407cf5484b4483688df9ecd0d25819d79bac
Agustin Pina <aguspina87@gmail.com>
2016-04-29 13:10:51 -0300

    Catching Interrupt outside subscribe

--- lib/mumukit/nuntius/consumer.rb
+++ lib/mumukit/nuntius/consumer.rb
@@ -6,14 +6,14 @@ class Mumukit::Nuntius::Consumer
       connection, channel, queue = Mumukit::Nuntius::Connection.start(queue_name)
       channel.prefetch(1)
 
-      queue.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
-        begin
+      begin
+        queue.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
           yield delivery_info, properties, JSON.parse(body).first
           channel.ack(delivery_info.delivery_tag)
-        rescue Interrupt => _
-          channel.close
-          connection.close
         end
+      rescue Interrupt => _
+        channel.close
+        connection.close
       end
     end
 	end
