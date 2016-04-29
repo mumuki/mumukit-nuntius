d5c10e003f9f9463733eefb72265850c3627f938
Agustin Pina <aguspina87@gmail.com>
2016-04-29 15:26:10 -0300

    Using persistent publish in publisher

--- lib/mumukit/nuntius/publisher.rb
+++ lib/mumukit/nuntius/publisher.rb
@@ -4,7 +4,7 @@ module Mumukit::Nuntius::Publisher
 
     def publish(queue_name, data)
       connection, channel, queue = Mumukit::Nuntius::Connection.start(queue_name)
-      channel.default_exchange.publish(data.to_json, :routing_key => queue_name, persistent: true)
+      channel.persistent_publish(data.to_json, queue_name)
       connection.close
     end
 
