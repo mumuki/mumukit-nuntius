d7a4a86b9fc6d88d34ab1c2f5e98cb6ddb355d48
Agustin Pina <aguspina87@gmail.com>
2016-04-29 15:26:28 -0300

    Add negative acknowledgment to consumer when it fails

--- lib/mumukit/nuntius/consumer.rb
+++ lib/mumukit/nuntius/consumer.rb
@@ -2,25 +2,27 @@ module Mumukit::Nuntius::Consumer
 
 	class << self
 
-    def start(queue_name)
+    def start(queue_name, &block)
       connection, channel, queue = Mumukit::Nuntius::Connection.start(queue_name)
       channel.prefetch(1)
 
       begin
-        subscribe queue, channel
+        subscribe queue, channel, &block
       rescue Interrupt => _
         channel.close
         connection.close
       end
     end
 
-    def subscribe(queue, channel)
+    def subscribe(queue, channel, &block)
       queue.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
         begin
-          yield delivery_info, properties, JSON.parse(body).first
+          block.call delivery_info, properties, JSON.parse(body).first
           channel.ack(delivery_info.delivery_tag)
         rescue => e
           Mumukit::Nuntius::Logger.warn "Failed to read body: #{e.message} \n #{e.backtrace}"
+          channel.persistent_publish(body, delivery_info.routing_key)
+          channel.nack(delivery_info.delivery_tag, requeue: true)
         end
       end
     end
