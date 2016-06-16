2ebbff451944ea576e238cf18dce29576d7edf4e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-06-16 16:12:09 -0300

    Adding logging

--- lib/mumukit/nuntius/consumer.rb
+++ lib/mumukit/nuntius/consumer.rb
@@ -3,19 +3,27 @@ module Mumukit::Nuntius::Consumer
 	class << self
 
     def start(queue_name, &block)
+      Mumukit::Nuntius::Logger.info "Attaching to queue #{queue_name}"
+
       connection, channel, queue = Mumukit::Nuntius::Connection.start(queue_name)
       channel.prefetch(1)
 
       begin
         subscribe queue, channel, &block
       rescue Interrupt => _
+        Mumukit::Nuntius::Logger.info "Leaving queue #{queue_name}"
+
         channel.close
         connection.close
       end
     end
 
     def subscribe(queue, channel, &block)
+      Mumukit::Nuntius::Logger.debug "Subscribed to queue #{queue}"
+
       queue.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
+        Mumukit::Nuntius::Logger.debug "Processing message #{body}"
+
         begin
           block.call delivery_info, properties, JSON.parse(body)
           channel.ack(delivery_info.delivery_tag)
