30b1f9ca6f96ab95220de0ab6584793fce0257c1
Agustin Pina <aguspina87@gmail.com>
2016-12-09 19:27:24 -0300

    Simplify hash notation

--- lib/mumukit/nuntius/event_publisher.rb
+++ lib/mumukit/nuntius/event_publisher.rb
@@ -3,7 +3,7 @@ module Mumukit::Nuntius::EventPublisher
   class << self
 
     def publish(event, payload)
-      Mumukit::Nuntius::Publisher.publish "events", { data: payload }.merge(type: event)
+      Mumukit::Nuntius::Publisher.publish "events", data: payload, type: event
     end
 
   end
