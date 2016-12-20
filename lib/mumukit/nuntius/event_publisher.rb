a0a6e89332b47561e58277218bcf4eb5d810117f
Agustin Pina <aguspina87@gmail.com>
2016-12-20 13:10:35 -0300

    Add sender to payload via env variable

--- lib/mumukit/nuntius/event_publisher.rb
+++ lib/mumukit/nuntius/event_publisher.rb
@@ -3,7 +3,8 @@ module Mumukit::Nuntius::EventPublisher
   class << self
 
     def publish(event, payload)
-      Mumukit::Nuntius::Publisher.publish "events", data: payload, type: event
+      payload.merge!(sender: ENV['MUMUKI_APPLICATION_NAME'])
+      Mumukit::Nuntius::Publisher.publish "events", { data: payload }, { type: event }
     end
 
   end
