62a7793227ead6524569b768987617bd1d10b730
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-01-22 10:23:28 -0300

    Documenting Nuntius publishin interface

--- lib/mumukit/nuntius.rb
+++ lib/mumukit/nuntius.rb
@@ -22,12 +22,36 @@ module Mumukit
       @config
     end
 
-    def self.notify!(queue_name, data)
-      notification_mode.notify! queue_name, data
+    # Notifies a message to a given queue.
+    #
+    # Messages are consumed using the Mumukit::Nuntius::Consumer module
+    #
+    # @param [String|Symbol] queue_name the name of the queue to which publish this message
+    # @param [Hash] message a json-like hash with the message data.
+    def self.notify!(queue_name, message)
+      notification_mode.notify! queue_name, message
     end
 
-    def self.notify_event!(data, type)
-      notification_mode.notify_event! data, type
+    # Notifies an event of the given type.
+    #
+    # Events are very similar to normal messages, with the following differences:
+    #
+    #   * they are published to a single queue, named `events`
+    #   * event data is augmented with the sender app name,
+    #     so that consumers can discard events sent from themselves.
+    #
+    # This makes events lighter and easier to send. You should send application
+    # events to here unless:
+    #
+    #   * you expect to send alot of those events
+    #   * you expect those events processing to be slow
+    #
+    # Events are consumed using the Mumukit::Nuntius::EventConsumer module
+    #
+    # @param [Hash] event a json-like hash with the event data
+    # @param [String|Symbol] type the type of event.
+    def self.notify_event!(event, type)
+      notification_mode.notify_event! event, type
     end
 
     private
