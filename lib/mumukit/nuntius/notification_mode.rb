e76f10e6236e2891713f0414eacd041eb58da825
Agustin Pina <aguspina87@gmail.com>
2017-03-03 11:15:22 -0300

    Fix from_env

--- lib/mumukit/nuntius/notification_mode.rb
+++ lib/mumukit/nuntius/notification_mode.rb
@@ -4,7 +4,7 @@ require 'mumukit/nuntius/notification_mode/nuntius'
 module Mumukit::Nuntius::NotificationMode
 
   def self.from_env
-    if ENV['MUMUKI_QUEUELESS_MODE'] || ENV['RACK_ENV'] == 'test' || ENV['RAILS_ENV'] == 'test'
+    if ENV['MUMUKI_QUEUELESS_MODE'].present? || ENV['RACK_ENV'] == 'test' || ENV['RAILS_ENV'] == 'test'
       Mumukit::Nuntius::NotificationMode::Deaf.new
     else
       Mumukit::Nuntius::NotificationMode::Nuntius.new
