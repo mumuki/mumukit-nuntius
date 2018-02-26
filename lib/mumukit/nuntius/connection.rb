924f43f170a3cb7f40b2e8f4aa3e7b964e1fa585
Lucho Cannavo <luchotc@hotmail.com.ar>
2018-02-26 09:41:11 -0300

    connection as instance variable

--- lib/mumukit/nuntius/connection.rb
+++ lib/mumukit/nuntius/connection.rb
@@ -7,18 +7,14 @@ class Mumukit::Nuntius::Connection
     end
 
     def start(exchange_name)
-      raise 'Nuntius connection isn\'t established' if connection.blank?
-      channel = connection.start.create_channel
+      raise 'Nuntius connection isn\'t established' unless @connection
+      channel = @connection.start.create_channel
       exchange = channel.fanout(exchange_name)
       [channel, exchange]
     end
 
     def establish_connection
-      @@connection = Bunny.new(host: config[:host], user: config[:user], password: config[:password])
-    end
-
-    def connection
-      @@connection ||= nil
+      @connection = Bunny.new(host: config[:host], user: config[:user], password: config[:password])
     end
   end
 end
